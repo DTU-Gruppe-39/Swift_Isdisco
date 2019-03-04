fb6bf50eb6a6f44050e9c9d935869d6eee2e7f9e
rallerG <raller799@live.dk>
2019-03-04 13:40:33 +0100

    Added tabs

---
+++ Isdisco/Isdisco/FourthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FourthViewController.swift
+//  Isdisco
+//
+//  Created by Rasmus Gregersen on 04/03/2019.
+//  Copyright © 2019 Rasmus Gregersen. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
---
+++ Isdisco/Isdisco/ThirdViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ThirdViewController.swift
+//  Isdisco
+//
+//  Created by Rasmus Gregersen on 04/03/2019.
+//  Copyright © 2019 Rasmus Gregersen. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
