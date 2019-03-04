b51a9496554927186aecfcf9f2e6856a537bc07c
rallerG <raller799@live.dk>
2019-03-04 13:22:23 +0100

    Initial commit

---
+++ Isdisco/Isdisco/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Isdisco
+//
+//  Created by Rasmus Gregersen on 04/03/2019.
+//  Copyright © 2019 Rasmus Gregersen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Isdisco/Isdisco/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Isdisco
+//
+//  Created by Rasmus Gregersen on 04/03/2019.
+//  Copyright © 2019 Rasmus Gregersen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
