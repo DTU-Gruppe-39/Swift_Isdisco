f42aa571e2dba6208254071a512498c41048fe7c
rallerG <raller799@live.dk>
2019-03-18 16:44:37 +0100

    Created layout for live list, still needs to be connected with code

---
+++ Isdisco/Isdisco/LiveTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LiveTableViewCell.swift
+//  Isdisco
+//
+//  Created by Rasmus Gregersen on 18/03/2019.
+//  Copyright © 2019 Rasmus Gregersen. All rights reserved.
+//
+
+import UIKit
+
+class LiveTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
