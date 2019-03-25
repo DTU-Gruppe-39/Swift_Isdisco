ddfb871e9522d428abec1adad1407e2894a9a840
Thomas Mattsson <tmattsson0@gmail.com>
2019-03-25 15:35:17 +0100

    quick fix

--- Isdisco/Isdisco/PopUpViewController.swift
+++ Isdisco/Isdisco/PopUpViewController.swift
@@ -2,7 +2,7 @@
 //  PopUpViewController.swift
 //  Isdisco
 //
-//  Created by Thomas Mattsson on 12/03/2019.
+//  Created by Thomas Mattsson on 25/03/2019.
 //  Copyright © 2019 Rasmus Gregersen. All rights reserved.
 //
 
@@ -12,39 +12,23 @@ class PopUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //self.view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
-        // self.showAnimate()
+
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func cancelPopUp(_ sender: UIButton) {
+    
+    @IBAction func cancelPopUp(_ sender: Any) {
         dismiss(animated: true)
     }
+    
+    /*
+    // MARK: - Navigation
 
-    
-    func showAnimate()
-    {
-        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
-        self.view.alpha = 0.0;
-        UIView.animate(withDuration: 0.15, animations: {
-            self.view.alpha = 1.0
-            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-        });
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    func removeAnimate()
-    {
-        UIView.animate(withDuration: 0.15, animations: {
-            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
-            self.view.alpha = 0.0;
-        }, completion:{(finished : Bool)  in
-            if (finished)
-            {
-                self.view.removeFromSuperview()
-            }
-        });
-    }
-  
+    */
 
 }
