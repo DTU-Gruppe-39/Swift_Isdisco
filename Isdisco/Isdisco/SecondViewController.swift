394d341a548184f5f867bcdf0280f032e48534f7
MagnusKochDTU <s175189@student.dtu.dk>
2019-04-29 21:09:11 +0200

    Fixed a crash in connection with search

--- Isdisco/Isdisco/SecondViewController.swift
+++ Isdisco/Isdisco/SecondViewController.swift
@@ -17,25 +17,21 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        setupTestArray()
     }
     
-    func setupTestArray () {
-        testArray.append(Singleton.Track(title: "Despacito" , artist: "Luis Fonsi", genre: "Pop", albumArt: "URL", trackId: 0))
-        testArray.append(Singleton.Track(title: "American Idiot", artist: "Green Day", genre: "Rock", albumArt: "URL", trackId: 1))
-        testArray.append(Singleton.Track(title: "7 Rings", artist: "Ariana Grande", genre: "Pop", albumArt: "URL", trackId: 2))
-    }
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return testArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let testArray = Singleton.shared.tracks[indexPath.row]
+        
         guard let trackCell = tableView.dequeueReusableCell(withIdentifier: "trackCell") as? TrackSearchTableCell else {
             return UITableViewCell()
         }
-        trackCell.song_name.text = testArray[indexPath.row].title
-        trackCell.artist.text = testArray[indexPath.row].title
+        trackCell.song_name.text = testArray.title
+        trackCell.artist.text = testArray.title
         trackCell.albumImage.image = UIImage(named:"LOGO_ISDISCO")
         
         return trackCell
