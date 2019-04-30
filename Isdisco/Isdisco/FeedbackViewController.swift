a979ea7ae4a3cb7ddf7798b86e8af37f65cc767e
Thomas Mattsson <tmattsson0@gmail.com>
2019-04-30 15:15:44 +0200

    Feedback

--- Isdisco/Isdisco/FeedbackViewController.swift
+++ Isdisco/Isdisco/FeedbackViewController.swift
@@ -81,31 +81,6 @@ class FeedbackViewController: UIViewController, UIPickerViewDataSource, UIPicker
     }
     
     func sendFeedback(){
-        //TODO: Send feedback to server
-        let parameters: [String: Any] = [
-            "IdQuiz" : 102,
-            "IdUser" : "iosclient",
-            "User" : "iosclient",
-            "List": [
-                [
-                    "IdQuestion" : 5,
-                    "IdProposition": 2,
-                    "Time" : 32
-                ],
-                [
-                    "IdQuestion" : 4,
-                    "IdProposition": 3,
-                    "Time" : 9
-                ]
-            ]
-        ]
-        
-        Alamofire.request("http://myserver.com", method: .post, parameters: parameters, encoding: JSONEncoding.default)
-            .responseJSON { response in
-                print(response)
-        }
+        //TODO: Add method to post JSON version of feedback.
     }
-    
-    
-    
 }
