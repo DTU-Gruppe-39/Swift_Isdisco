5a1963e7821c87ce002d2b6873ec67a797b8f674
rallerG <raller799@live.dk>
2019-05-12 00:02:45 +0200

    Changed Json in music request

--- Isdisco/Isdisco/Model/Musicrequest.swift
+++ Isdisco/Isdisco/Model/Musicrequest.swift
@@ -15,9 +15,9 @@ class Musicrequest {
     let userId: Int
     let timestamp: String  //Should probably be milliseconds and then calculated to min/hours as an Int insted of a double
     let upvotes: [Int]
-    let downvotes: Int
+    let downvotes: [Int]
     
-    init(id: Int, track: Track,userId: Int, timestamp: String, downvotes: Int,upvotes: [Int]) {
+    init(id: Int, track: Track,userId: Int, timestamp: String, downvotes: [Int],upvotes: [Int]) {
         self.id = id
         self.track = track
         self.userId = userId
@@ -32,22 +32,26 @@ class Musicrequest {
         self.track = track;
         self.userId = userId;
         self.timestamp = ""
-        self.downvotes = 0
+        self.downvotes = [Int]()
         self.upvotes = [Int]()
     }
     
     static func jsonToObject (json: JSON) -> Musicrequest {
         var upvotesArray = [Int]()
+        var downvotesArray = [Int]()
         for upvote in json["upvotes"].arrayValue {
             upvotesArray.append(upvote.intValue)
         }
+        for downvote in json["downvotes"].arrayValue {
+            downvotesArray.append(downvote.intValue)
+        }
         
-        return Musicrequest(id: json["id"].intValue,track: Track.jsonToObject(json: json["track"]),userId: json["userId"].intValue,timestamp: json["timestamp"].stringValue,downvotes: json["downvotes"].intValue,upvotes: upvotesArray)
+        return Musicrequest(id: json["id"].intValue,track: Track.jsonToObject(json: json["track"]),userId: json["userId"].intValue,timestamp: json["timestamp"].stringValue,downvotes: downvotesArray,upvotes: upvotesArray)
     }
 
     static func objectToJson (object: Musicrequest) -> [String: Any] {
         return ["track":
                     Track.objectToJson(object: object.track),
-                "userid":1]
+                "userid":Singleton.shared.currentUserId]
     }
 }
